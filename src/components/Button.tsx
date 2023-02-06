ecb08ad181a9504236b04a1b7267d8812a5e6629
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2023-02-06 16:28:19 -0300

    :sparkles: Adiciona o cursor pointer

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 export function Button({ keyButton, type }: ButtonProps){
   console.log(keyButton)
   return(
-    <span className={clsx('w-[64px] h-[64px] rounded-full flex justify-center items-center shadow-black shadow-sm', {
+    <span className={clsx('w-[64px] h-[64px] rounded-full flex justify-center items-center shadow-black shadow-sm cursor-pointer', {
       'bg-blue-900': type === 'operation',
       'bg-blue-500': type === 'equal',
       'text-clear': type === 'clear'
